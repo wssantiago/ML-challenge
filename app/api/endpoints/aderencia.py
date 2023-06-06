7556fc351ea5b73bd447fda0bb3356fabb6d8f48
Williams Santiago <wssf@cin.ufpe.br>
2023-06-06 01:14:17 -0300

    Catch exception in adherence endpoint

--- app/api/endpoints/aderencia.py
+++ app/api/endpoints/aderencia.py
@@ -19,24 +19,28 @@ def calc_adherence(model_name: str, json: dict):
     logging.info(
         "Calculating the adherence over the reference training base and the requested input base...")
     if model_name == 'classifier':
-        model = read_model(model_name)
+        try:
+            model = read_model(model_name)
 
-        X_ref, _ = split_df(json['reference'])
-        X_req, _ = split_df(json['request'])
+            X_ref, _ = split_df(json['reference'])
+            X_req, _ = split_df(json['request'])
 
-        # Defining the score distribution for both datasets
-        logging.info(
-            "Predicting scores distribution for both reference and input bases...")
-        score_ref = model.predict_proba(X_ref)
-        score_req = model.predict_proba(X_req)
+            # Defining the score distribution for both datasets
+            logging.info(
+                "Predicting scores distribution for both reference and input bases...")
+            score_ref = model.predict_proba(X_ref)
+            score_req = model.predict_proba(X_req)
 
-        # The scores are passed as parameters to this scipy function.
-        # It executes the KS test and returns the metrics relative
-        # to either being samples from different distributions or
-        # from the same one.
-        distance = ks_2samp(score_ref[:, 1], score_req[:, 1])
-        logging.info(
-            "Successfully performed the Kolmogorov-Smirnov test over both score distributions using scipy...")
+            # The scores are passed as parameters to this scipy function.
+            # It executes the KS test and returns the metrics relative
+            # to either being samples from different distributions or
+            # from the same one.
+            distance = ks_2samp(score_ref[:, 1], score_req[:, 1])
+            logging.info(
+                "Successfully performed the Kolmogorov-Smirnov test over both score distributions using scipy...")
 
-        return {'KStest-result': {'statistic': distance[0], 'p-value': distance[1]}}
+            return {'KStest-result': {'statistic': distance[0], 'p-value': distance[1]}}
+        except (KeyError, ValueError) as err:
+            logging.error(
+                "KeyError or ValueError exception raised while trying to define adherence metrics")
     return {'Model version': 'not supported yet...'}
